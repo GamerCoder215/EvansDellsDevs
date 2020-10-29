6638ae85a24f126ba6be28b24330b3f00d3762cd
repl.it user <replituser@example.com>
2020-10-29 19:05:04 +0000

    removed_files

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (client) => {
-  console.log('Cammie is currently running correctly.');
-	const Discord = require('discord.js')
-  // Set the client user's activity
-client.user.setActivity('Connor Corner', { type: 'WATCHING' })
-  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-  .catch(console.error)
-};
